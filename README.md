1b67a5a65878cb59901e79a7c0fa686582a54a8e
shruthi <shruthitupakulakrt@gmail.com>
2020-03-21 20:57:05 +0530

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-## Instabook
-
-This is my first react application, developed with inspiration from instagram. This Application uses JSON Placeholder as its backend.
--- src/utils.js
+++
@@ -1,8 +0,0 @@
-export const getRandomColor = () => {
-  var letters = "6789ABCDEF".split("");
-  var color = "#";
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * letters.length)];
-  }
-  return color;
-};
